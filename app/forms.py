14fb77b1412df5cef1116b8bc564d7434604f6ed
Baotran <bt@baotrans-air.lan>
2022-12-04 16:04:48 -0800

    Completed edit profile, added html for edit profile, and started on user profile

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, ValidationError, Length
 from app.models import User
+from flask import redirect, flash
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -18,7 +19,31 @@ class RegisterForm(FlaskForm):
      repeatPassword = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
      submit = SubmitField('Register')
 
+    #checks if username is available
      def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
             raise ValidationError('Please use a different username.')
+
+class HomeForm(FlaskForm):
+    message = StringField('enter message', validators=[DataRequired()])
+    post = SubmitField('Post')
+
+class EditForm(FlaskForm):
+    pic = FileField('Profile Picture')
+    username = StringField('Username')
+    password = PasswordField('New Password')
+    confirmPassword = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Save changes')
+
+    #checks if username is available
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+        
+    
+
+
+        
--- app/models.py
+++ app/models.py
@@ -14,7 +14,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String, unique=True)
     password = db.Column(db.String(200))
     email = db.Column(db.String(32), unique=True)
-    
+    posts = db.relationship('Post', backref='author', lazy=True)
+
     followers = db.Table('followers', 
                 db.Column('follower_id', db.Integer, db.ForeignKey('user.id')), 
                 db.Column('followed_id', db.Integer, db.ForeignKey('user.id')))
@@ -29,6 +30,12 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    def get_password(self):
+        return self.password
+        
+    def set_username(self, username):
+        self.username = username
 
     def __repr__(self):
         return f'<User {self.username}>'
